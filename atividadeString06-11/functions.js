727c444f174bc94133bb484db5c012959a398778
Yan Victor <ybritogomes@gmail.com>
2023-11-09 17:57:16 -0300

    feat(stringassignment): solved ex 17-25

--- atividadeString06-11/functions.js
+++ atividadeString06-11/functions.js
@@ -143,6 +143,137 @@ function ex16(text1, text2) {
 }
 
 // 17. Recebe uma string S e inteiros na˜o-negativos I e J e imprima o segmento S[I..J].
-function ex17() {
+function ex17(s, i, j) {
+    let segmento = ''
+
+    for(i; i <= j; i++) {
+        segmento += s.split('')[i]
+    }
+
+    console.log(`Segmento: ${segmento}`)
+}
+
+// 18. Recebe  do  usua´rio  uma  string  S,  um  caractere  C,  e  uma posic¸a˜o I e devolve o ´ındice da primeira posic¸a˜o da string onde foi encontrado o caractere C. A procura deve comec¸ar a partir da posic¸a˜o I.
+function ex18() {
+    let s = prompt('Digite a frase desejada: ')
+    let c = prompt('Digite o carácter que deseja procurar: ')
+    let i = parseInt(prompt('Digite o valor do index: '))
+
+    s = s.split('')
+    for(i; i < s.length; i++) {
+        if(s[i] == c) {
+            console.log(`Index do carácter procurado: ${i}`)
+            return
+        }
+    }
+    console.log('Carácter não encontrado com as especificações.')
+}
+
+// 19. leia duas palavras e diga qual deles vem primeiro na ordem alfabe´tica. Dica: ‘a’ e´ menor do que ‘b’.
+function ex19(palavra1, palavra2) {
+    console.log(`A palavra que vem primeiro é a: ${[palavra1, palavra2].sort()[0]}`)
+}
+
+// 20. O co´digo de Ce´sar e´ uma das mais simples e conhecidas te´cnicas de criptografia. E´  um tipo de substituic¸a˜o na qual cada letra do texto e´ substitu´ıda por outra, que se apresenta no alfabeto abaixo dela um nu´mero fixo de vezes.  Por exemplo, com uma troca de treˆs posic¸o˜es, ‘A’ seria substitu´ıdo por ‘D’, ‘B’ se tornaria ‘E’, e assim por diante. Fac¸a uso desse Co´digo de Ce´sar (3 posic¸o˜es), entre com uma string e retorne a string codificada. Exemplo:
+function ex20(text) {
+    let dictCesar = {
+        'a': 'd',
+        'b': 'e',
+        'c': 'f',
+        'd': 'g',
+        'e': 'h',
+        'f': 'i',
+        'g': 'j',
+        'h': 'k',
+        'i': 'l',
+        'j': 'm',
+        'k': 'n',
+        'l': 'o',
+        'm': 'p',
+        'n': 'q',
+        'o': 'r',
+        'p': 's',
+        'q': 't',
+        'r': 'u',
+        's': 'v',
+        't': 'w',
+        'u': 'x',
+        'v': 'y',
+        'w': 'z',
+        'x': 'a',
+        'y': 'b',
+        'z': 'c'
+    }
     
+    let stringSeparada = text.split('')
+
+    for(let i = 0; i < stringSeparada.length; i++) {
+        if(/[A-Z]/i.test(stringSeparada[i])) {
+            stringSeparada[i] = dictCesar[stringSeparada[i].toLowerCase()]
+        }
+    }
+
+    console.log(`Frase transformada: ${stringSeparada.join('')}`)
+}
+
+// 21. Dada uma string, diga se ela e´  um pal´ındromo ou na˜o.  Lembrando que um pal´ındromo e´  uma palavra que tenha a propriedade de poder ser lida tanto da direita para a esquerda como da esquerda para a direita. 
+function ex21(text) {
+    let textSeparada = text.split(''), textSeparadaInv = text.split('').reverse()
+
+    for(let i = 0; i < textSeparada.length; i++) {
+        if(textSeparada[i] != textSeparadaInv[i]) {
+            console.log('Não é um palíndromo.')
+            return
+        }
+    }
+
+    console.log('É um palíndromo.')
+}
+
+// 22.Leia duas strings fornecidas pelo usua´rio e verifique se a se- gunda string lida esta contida no final da primeira, retornando o resultado da verificac¸a˜o.
+function ex22(string1, string2) {
+    let string1sep = string1.trim().split(' ')
+    let exp = `${string2}\\b`
+    let re = new RegExp(exp, 'gi')
+    console.log(re)
+    if(re.test(string1sep[string1sep.length - 1])) {
+        console.log('O segundo texto aparece no final do primeiro texto.')
+        return
+    }
+    console.log('O final do primeiro texto não condiz com o segundo texto')
+}
+
+function ex23(str1, str2, n) {
+    str2sep = str2.split('')
+    for(let i = 0; i < n; i++) {
+        str1 += str2[i]
+    }
+    str1 += '\\0'
+    console.log(`Str1: ${str1}`)
+}
+
+function ex24(a, b) {
+    let re = new RegExp(b, 'gi')
+    let comb = [...a.matchAll(re)]
+    console.log(`Quantidade de vezes que a string 'b' estava em 'a': ${comb.length}`)
+}
+
+// 25. Leia uma cadeia de caracteres no formato “DD/MM/AAAA” e copie o dia, meˆs e ano para 3 varia´veis inteiras. Antes disso, verifique se as barras esta˜o no lugar certo, e se DD, MM e AAAA sa˜o nume´ricos.
+function ex25(date) {
+    dateSplit = date.split('/')
+
+    let re = /[^0-9]/
+    if(re.test(dateSplit[0]) || re.test(dateSplit[1]) || re.test(dateSplit[2]) || date.split('')[2] != '/' || date.split('')[5] != '/') {
+        console.log('Data inválida!')
+        return
+    }
+
+    let dia = dateSplit[0], mes = dateSplit[1], ano = dateSplit[2]
+
+    if(ano.split('').length != 4) {
+        console.log('Data inválida!')
+        return
+    }
+
+    console.log(`Dia: ${dia}\nMês: ${mes}\nAno: ${ano}`)
 }
